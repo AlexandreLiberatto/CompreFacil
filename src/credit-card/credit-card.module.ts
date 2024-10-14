4cd021b1e87ec1b49fe02f37538bc147fb886d07
Alexandre liberato <alexandreliberatto@gmail.com>
2024-10-14 17:53:08 -0300

    Criando métodos para enviar mensagem e confirmar o envio
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- src/credit-card/credit-card.module.ts
+++ src/credit-card/credit-card.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { CreditCardService } from './credit-card.service';
 import { CreditCardController } from './credit-card.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices'; // Importando o Transport
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NOTIFICATION_SERVICE',
+        transport: Transport.RMQ, // Usando o transport para RMQ
+        options: {
+          urls: ['amqp://admin:123456@localhost'],
+          queue: 'notification',
+          noAck: true,
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [CreditCardController],
   providers: [CreditCardService],
 })
